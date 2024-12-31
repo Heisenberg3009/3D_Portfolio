95b8b926abe7c5aaee79c9efb2d965b1d96dba49
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2024-12-31 12:38:40 +0530

    Twenty Third Commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -53,13 +53,36 @@ const About = () => {
         </div>
         <div className="mt-12 flex">
           <VerticalTimeline>
-            {experiences.map (() => (
-              <VerticalTimelineElement>
+            {experiences.map ((experience) => (
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                icon={
+                 <div>
+                  <img 
+                    src={experience.icon}
+                    alt={experience.company_name}
+                    className="w-[60%] h-[60%] object-contain"
+                  />
+                 </div> 
+                }
+              >
                 <div>
-                  <h3>
-                    {experiences.title}
+                  <h3 className="text-black text-xl font-poppins font-semibold">
+                    {experience.title}
                   </h3>
+                  <p className="text-black-500 font-medium font-base" style={{margin: 0}}>
+                    {experience.company_name}
+                  </p>
                 </div>
+                <ul className="my-5 list-disc ml-5 space-y-2">
+                  {experience.points.map((point, index) => (
+                    <li key = {`experience-point-${index}`} 
+                    className="text-black-500/50 font-normal pl-1 text-sm">
+                      {point}
+                    </li>
+                  ))}
+                </ul>
               </VerticalTimelineElement>
             ))}
           </VerticalTimeline>
